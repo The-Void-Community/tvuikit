ef6c17ddf511ddc75b09d1b205c1a8a7c5be6e14
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-10-24 09:29:20 +0500

    -add button types

--- src/components/button/index.stories.ts
+++ src/components/button/index.stories.ts
@@ -1,16 +1,66 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
+
 import { Button } from ".";
+import { button } from "../../variables/colors/button";
 
 const meta: Meta<typeof Button> = {
   component: Button,
   tags: ["autodocs"],
 };
 
+const defaultArgumentTypes: StoryObj<typeof Button>["argTypes"] = {
+  children: {
+    type: "string"
+  },
+
+  type: {
+    type: "string",
+    options: Object.keys(button),
+  
+    control: {
+      type: "select"
+    }
+  }
+};
+
 export const Default: StoryObj<typeof Button> = {
   args: {
-    text: "I'm button",
+    children: "I'm button",
     type: "default"
   },
+  argTypes: defaultArgumentTypes
+};
+
+export const Primary: StoryObj<typeof Button> = {
+  args: {
+    children: "I'm button",
+    type: "primary"
+  },
+  argTypes: defaultArgumentTypes
+};
+
+export const Danger: StoryObj<typeof Button> = {
+  args: {
+    children: "I'm button",
+    type: "danger"
+  },
+  argTypes: defaultArgumentTypes
+};
+
+export const Secondary: StoryObj<typeof Button> = {
+  args: {
+    children: "I'm button",
+    type: "secondary"
+  },
+  argTypes: defaultArgumentTypes
+};
+
+export const Tetriary: StoryObj<typeof Button> = {
+  args: {
+    children: "I'm button",
+    type: "tetriary"
+  },
+  argTypes: defaultArgumentTypes
 };
 
 export default meta;
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,23 +2,21 @@ import type { ReactNode } from "react"
 
 type Props = {
   children: ReactNode,
-  text: string,
   type: string
 }
 
 export const Button = ({
   children,
-  text,
   type
 }: Props) => {
   return (
     <button
       className={[
-        "bg-(--fg-card) px-8 py-4 rounded-lg",
+        "bg-(--fg-card) px-8 py-4 rounded-lg text-(--btn-${type})",
         `text-(--btn-${type})`
       ].join(" ")}
     >
-      {children || text}
+      {children}
     </button>
   )
 }
