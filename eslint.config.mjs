ca9ef14911520fc0dd4c005a3c02d985566b66b1
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-11-18 12:42:01 +0500

    -style

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,26 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint, { parser } from "typescript-eslint";
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: ["**/*.js", "**/*.d.ts", "**/dist/**", "**/node_modules/**"]
+    ignores: ["**/*.js", "**/*.d.ts", "**/dist/**", "**/node_modules/**"],
   },
 
   {
     languageOptions: {
       globals: globals.es2024,
-      parser: parser
-    }
+      parser: parser,
+    },
   },
 
   {
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
 
     rules: {
