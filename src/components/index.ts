1788e3fdff4dd8c8739b17a8f4034728b2924271
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2026-01-04 11:55:53 +0500

    -add workflows

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,10 @@
-export * from "./button";
-export * from "./background";
-export * from "./input";
-export * from "./wrapper";
-export * from "./modal";
 export * from "./active";
+export * from "./background";
+export * from "./button";
 export * from "./checkbox";
+export * from "./input";
+export * from "./modal";
 export * from "./switch";
+export * from "./wrapper";
 
 export * as Guildline from "./guideline";
