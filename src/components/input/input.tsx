cbd1c38ef6b7c4bdff0fc9691d38daec8d09f6c7
FOCKUSTY <wwddwwwd@mail.ru>
2026-01-10 17:08:53 +0500

    -up textarea

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -2,20 +2,27 @@ import type { DetailedHTMLProps, InputHTMLAttributes } from "react";
 
 import { cn } from "../../utils/cn";
 
-export type InputProps = DetailedHTMLProps<
+export type InputProps = {
+  overwriteClassName?: boolean
+} & DetailedHTMLProps<
   InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
 >;
 
-export const Input = ({ className, ...props }: InputProps) => {
+export const Input = ({ className, overwriteClassName, ...props }: InputProps) => {
+  const finalClassName = overwriteClassName
+    ? className
+    : cn(
+      "bg-(--bg-card) px-4 py-2 rounded-lg min-w-30 max-w-60",
+      "focus:outline-0",
+      className,
+    );
+
   return (
     <input
       placeholder="input here..."
       {...props}
-      className={cn(
-        "bg-(--bg-card) px-4 py-2 rounded-lg min-w-30 max-w-60",
-        className,
-      )}
+      className={finalClassName}
     />
   );
 };
--- src/components/input/textarea.tsx
+++ src/components/input/textarea.tsx
@@ -1,21 +1,52 @@
-import type { DetailedHTMLProps, TextareaHTMLAttributes } from "react";
+import type { ChangeEvent, DetailedHTMLProps, TextareaHTMLAttributes } from "react";
 
 import { cn } from "../../utils/cn";
 
-export type TextareaProps = DetailedHTMLProps<
+export type TextareaProps = {
+  overwriteClassName?: boolean;
+  minimalRowsHeight?: number;
+  maximalRowsHeight?: number;
+} & DetailedHTMLProps<
   TextareaHTMLAttributes<HTMLTextAreaElement>,
   HTMLTextAreaElement
 >;
 
-export const Textarea = ({ className, ...props }: TextareaProps) => {
+export const Textarea = ({
+  className,
+  overwriteClassName = false,
+  minimalRowsHeight = 1,
+  maximalRowsHeight = 10,
+  onChange,
+  ...props
+}: TextareaProps) => {
+  const finalClassName = overwriteClassName
+    ? className
+    : cn(
+        "bg-(--bg-card) px-4 py-2 rounded-lg min-w-30 max-w-150 min-h-10 resize",
+        "focus:outline-0",
+        className,
+      );
+
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    const element = event.currentTarget;
+    element.style.height = "auto";
+
+    const lineHeight = parseInt(getComputedStyle(element).lineHeight);
+    const newHeight = Math.min(
+      Math.max(element.scrollHeight, minimalRowsHeight * lineHeight),
+      maximalRowsHeight * lineHeight
+    );
+    element.style.height = `${newHeight}px`;
+
+    onChange?.(event);
+  }
+
   return (
     <textarea
       placeholder="write here..."
+      className={finalClassName}
+      onChange={handleChange}
       {...props}
-      className={cn(
-        "bg-(--bg-card) px-4 py-2 rounded-lg min-w-30 max-w-150 min-h-10 resize",
-        className,
-      )}
     />
   );
 };
